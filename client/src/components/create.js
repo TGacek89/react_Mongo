c0345934a6e7c2c13396eb86f453570d7097d798
TG89ef <tomasz.gacek@ef.uk.com>
2023-09-12 11:30:22 +0200

    CRUD operations

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -1,97 +1,60 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 
 export default function Create() {
-  const [form, setForm] = useState({
+  const [book, setBook] = useState({
     title: "",
     description: "",
-    price: "",
+    price: null,
     cover: "",
   });
   const navigate = useNavigate();
 
-  // These methods will update the state properties.
-  function updateForm(value) {
-    return setForm((prev) => {
-      return { ...prev, ...value };
-    });
-  }
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  // This function will handle the submission.
-  async function onSubmit(e) {
+  const handleSend = async (e) => {
     e.preventDefault();
+    try {
+      await axios.post("http://localhost:5050/record", book);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    const newPerson = { ...form };
-
-    await fetch("http://localhost:5050/record", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newPerson),
-    }).catch((error) => {
-      window.alert(error);
-      return;
-    });
-
-    setForm({ title: "", description: "", price: "", cover: "" });
-    navigate("/");
-  }
-
-  // This following section will display the form that takes the input from the user.
   return (
-    <div>
-      <h3>Create New Record</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={form.name}
-            onChange={(e) => updateForm({ title: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description">Description</label>
-          <input
-            type="text"
-            className="form-control"
-            id="description"
-            value={form.description}
-            onChange={(e) => updateForm({ description: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            className="form-control"
-            id="price"
-            value={form.price}
-            onChange={(e) => updateForm({ price: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="cover">Cover</label>
-          <input
-            type="text"
-            className="form-control"
-            id="cover"
-            value={form.cover}
-            onChange={(e) => updateForm({ cover: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="submit"
-            value="Create person"
-            className="btn btn-primary"
-          />
-        </div>
-      </form>
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={handleChange}
+        name="description"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button className="addBook" onClick={handleSend}>
+        ADD
+      </button>
     </div>
   );
 }
